9d666ac093e7f89dea1babd291b29ed4f420c8ac
soheil najafi <soheilnajafi57@gmail.com>
2018-07-16 15:34:55 -0400

    My springboot jpa files

---
+++ src/main/java/com/company/springboot/SpringBoot_JPA/App.java
@@ -0,0 +1,13 @@
+package com.company.springboot.SpringBoot_JPA;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
